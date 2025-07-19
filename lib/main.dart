fef40ec4cfce4425b9dd71966089febf2425ef62
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2025-07-18 19:54:18 -0500

    stream temporizador

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:con2g12/firebase_options.dart';
 import 'package:con2g12/pages/home_page.dart';
+import 'package:con2g12/pages/temporizador_stream_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(MaterialApp(home: HomePage(), debugShowCheckedModeBanner: false));
+  runApp(
+    MaterialApp(
+      home: TemporizadorStreamPage(),
+      debugShowCheckedModeBanner: false,
+    ),
+  );
 }
